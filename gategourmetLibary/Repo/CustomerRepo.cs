153ef458f4c2e7371ab604cc6d7579e3478a1bcb
Mohamad Nour Abo Jamil <moa010@edu.zealand.dk>
2025-11-24 13:31:36 +0100

    fg

--- gategourmetLibary/Repo/CustomerRepo.cs
+++ gategourmetLibary/Repo/CustomerRepo.cs
@@ -7,36 +7,38 @@ using System.Threading.Tasks;
 
 namespace gategourmetLibrary.Repo
 {
-    internal class CustomerRepo
+    public class CustomerRepo
     {
-
-
-        public void Add(int customer)
-        {
-
-        }
-
         public int Get()
         {
             int i = 1;
             return i;
         }
 
+        public void Add(int customer)
+        {
+
+        }
+
         public void Delete(int customer)
         {
+
+        }
+        public void Update(int customer)
+        {
+
         }
         public void GetAll()
         {
-        }
-        public void MyOrdwer (int customer)
-        {
-        }
 
-        public void Update(int customer) 
+        }
+        public void MyOrder (int customer)
         {
+
         }
         public void Filter (string customer)
         {
+
         }
 
     }
