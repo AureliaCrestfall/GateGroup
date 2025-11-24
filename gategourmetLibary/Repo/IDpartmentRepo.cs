3501afcc9898d05df16645bb52f1cd3a89622879
Mohamad Nour Abo Jamil <moa010@edu.zealand.dk>
2025-11-24 12:58:02 +0100

    kjn

--- gategourmetLibary/Repo/IDpartmentRepo.cs
+++ gategourmetLibary/Repo/IDpartmentRepo.cs
@@ -8,20 +8,20 @@ namespace gategourmetLibrary.Repo
 {
     public interface IDpartmentRepo
     {
-        //    void Add(int department);
-        //    void Delete(int department);
-        //    void GetAll();
-        //    void Update(int DepartmentID, Department UpdateDepartment);
-        //    void NewWarehouse(WereHous newwerehous);
-        //    void Stoklngredient(Ingredient stocklngredient);
-        //    void GetWerehousStock(int werehous);
-        //    void GetDepartmentManagers(int department);
-        //    void GetDepartmentEmployees(int department);
-        //    int Get(int DepartmentID);
-        //    void AddNewDepartmentManager(int DepartmentID, manager newManager);
-        //    void AddnewDepartmentEmpolyee(int DepartmentID, Employee newEmployee);
-        //    void RemoveStock(Ingredient Ingredient, int amount, Department departmentID, Warehouse warehouseID);
+        //void Add(int department);
+        //void Delete(int department);
+        //void GetAll();
+        //void Update(int DepartmentID, Department UpdateDepartment);
+        //void NewWarehouse(WereHous newwerehous);
+        //void Stoklngredient(Ingredient stocklngredient);
+        //void GetWerehousStock(int werehous);
+        //void GetDepartmentManagers(int department);
+        //void GetDepartmentEmployees(int department);
+        //int Get(int DepartmentID);
+        //void AddNewDepartmentManager(int DepartmentID, manager newManager);
+        //void AddnewDepartmentEmpolyee(int DepartmentID, Employee newEmployee);
+        //void RemoveStock(Ingredient Ingredient, int amount, Department departmentID, Warehouse warehouseID);
 
-        //}
     }
+}
 }
